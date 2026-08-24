a1e808261ae26bb2d3a827c8e0bfc1dec57c57cd
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2026-08-24 13:56:31 +0600

    Create GameState.kt

---
+++ app/src/main/java/com/rana/ludo/model/GameState.kt
@@ -0,0 +1,12 @@
+package com.rana.ludo.model
+
+data class GameState(
+
+    val players: MutableList<Player>,
+
+    var currentPlayerIndex: Int = 0,
+
+    var diceValue: Int = 0,
+
+    var winner: PlayerColor? = null
+)
