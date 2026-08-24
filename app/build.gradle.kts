aeff58a9f647b4a82a79c5dcd8b2578a6c0e1db9
mr-masudrana <mr-masudrana@users.noreply.github.com>
2026-08-24 16:50:07 +0600

    Fix Java Kotlin JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,18 @@ android {
         compose = true
     }
 
+    /*
+     * Java এবং Kotlin দুটোই JVM 17
+     */
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -34,13 +46,23 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2025.01.00"
+        )
+    )
 
     implementation("androidx.compose.ui:ui")
 
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
 
-    implementation("androidx.compose.material3:material3")
+    implementation(
+        "androidx.compose.material3:material3"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 }
